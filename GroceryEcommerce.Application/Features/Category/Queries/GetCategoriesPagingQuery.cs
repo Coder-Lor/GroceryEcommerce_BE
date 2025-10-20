3e7f4374169baae45ef6d65f2e106307e892a0cf
ThangIT2k4 <bboybt2k4@gmail.com>
2025-10-20 12:18:50 +0700

    add queries for checking category existence by name and paging categories

--- GroceryEcommerce.Application/Features/Category/Queries/GetCategoriesPagingQuery.cs
+++ GroceryEcommerce.Application/Features/Category/Queries/GetCategoriesPagingQuery.cs
@@ -5,11 +5,5 @@ using MediatR;
 namespace GroceryEcommerce.Application.Features.Category.Queries;
 
 public record GetCategoriesPagingQuery(
-    int Page = 1,
-    int PageSize = 20,
-    string? SearchTerm = null,
-    string? SortBy = "Name",
-    string? SortDirection = "Asc",
-    short? Status = null,
-    Guid? ParentCategoryId = null
-) : IRequest<Result<PagedResult<CategoryBaseResponse>>>;
+    PagedRequest Request
+) : IRequest<Result<PagedResult<CategoryDto>>>;
