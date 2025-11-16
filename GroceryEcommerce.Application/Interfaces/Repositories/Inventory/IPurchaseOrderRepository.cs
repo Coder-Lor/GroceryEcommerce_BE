f52506ed04b19348082fbfe5298278d47cf6e32a
ThangIT2k4 <bboybt2k4@gmail.com>
2025-11-16 16:24:59 +0700

    Refactor IRepository

--- GroceryEcommerce.Application/Interfaces/Repositories/Inventory/IPurchaseOrderRepository.cs
+++ GroceryEcommerce.Application/Interfaces/Repositories/Inventory/IPurchaseOrderRepository.cs
@@ -1,23 +1,25 @@
 using GroceryEcommerce.Application.Common;
+using GroceryEcommerce.Application.Interfaces.Repositories.Common;
 using GroceryEcommerce.Domain.Entities.Inventory;
 
 namespace GroceryEcommerce.Application.Interfaces.Repositories.Inventory;
 
-public interface IPurchaseOrderRepository
+public interface IPurchaseOrderRepository : IPagedRepository<PurchaseOrder>
 {
     // Basic CRUD operations
     Task<Result<PurchaseOrder?>> GetByIdAsync(Guid purchaseOrderId, CancellationToken cancellationToken = default);
     Task<Result<PurchaseOrder?>> GetByNumberAsync(string orderNumber, CancellationToken cancellationToken = default);
-    Task<Result<PagedResult<PurchaseOrder>>> GetPagedAsync(PagedRequest request, CancellationToken cancellationToken = default);
-    Task<bool> CreateAsync(PurchaseOrder purchaseOrder, CancellationToken cancellationToken = default);
-    Task<bool> UpdateAsync(PurchaseOrder purchaseOrder, CancellationToken cancellationToken = default);
-    Task<bool> DeleteAsync(Guid purchaseOrderId, CancellationToken cancellationToken = default);
+    Task<Result<bool>> CreateAsync(PurchaseOrder purchaseOrder, CancellationToken cancellationToken = default);
+    Task<Result<bool>> UpdateAsync(PurchaseOrder purchaseOrder, CancellationToken cancellationToken = default);
+    Task<Result<bool>> DeleteAsync(Guid purchaseOrderId, CancellationToken cancellationToken = default);
     
     // Purchase order management operations
     Task<Result<bool>> ExistsAsync(Guid purchaseOrderId, CancellationToken cancellationToken = default);
-    Task<Result<PagedResult<PurchaseOrder>>> GetByStatusAsync(short status, PagedRequest pagedRequest, CancellationToken cancellationToken = default);
-    Task<Result<PagedResult<PurchaseOrder>>> GetByDateRangeAsync(DateTime fromDate, DateTime toDate, PagedRequest pagedRequest, CancellationToken cancellationToken = default);
     Task<Result<string>> GenerateOrderNumberAsync(CancellationToken cancellationToken = default);
     Task<Result<bool>> UpdateStatusAsync(Guid purchaseOrderId, short status, CancellationToken cancellationToken = default);
+    
+    // Paged queries
+    Task<Result<PagedResult<PurchaseOrder>>> GetByStatusAsync(short status, PagedRequest pagedRequest, CancellationToken cancellationToken = default);
+    Task<Result<PagedResult<PurchaseOrder>>> GetByDateRangeAsync(DateTime fromDate, DateTime toDate, PagedRequest pagedRequest, CancellationToken cancellationToken = default);
     Task<Result<PagedResult<PurchaseOrder>>> GetPendingOrdersAsync(PagedRequest pagedRequest, CancellationToken cancellationToken = default);
 }
--- GroceryEcommerce.Application/Interfaces/Repositories/Sales/IOrderPaymentRepository.cs
+++ GroceryEcommerce.Application/Interfaces/Repositories/Sales/IOrderPaymentRepository.cs
@@ -1,25 +1,25 @@
 using GroceryEcommerce.Application.Common;
+using GroceryEcommerce.Application.Interfaces.Repositories.Common;
 using GroceryEcommerce.Domain.Entities.Sales;
 
 namespace GroceryEcommerce.Application.Interfaces.Repositories.Sales;
 
-public interface IOrderPaymentRepository
+public interface IOrderPaymentRepository : IPagedRepository<OrderPayment>
 {
     // Basic CRUD operations
     Task<Result<OrderPayment?>> GetByIdAsync(Guid paymentId, CancellationToken cancellationToken = default);
     Task<Result<List<OrderPayment>>> GetByOrderIdAsync(Guid orderId, CancellationToken cancellationToken = default);
     Task<Result<OrderPayment?>> GetByTransactionIdAsync(string transactionId, CancellationToken cancellationToken = default);
-    Task<Result<PagedResult<OrderPayment>>> GetPagedAsync(PagedRequest request, CancellationToken cancellationToken = default);
-    Task<Result<OrderPayment>> CreateAsync(OrderPayment payment, CancellationToken cancellationToken = default);
+    Task<Result<bool>> CreateAsync(OrderPayment payment, CancellationToken cancellationToken = default);
     Task<Result<bool>> UpdateAsync(OrderPayment payment, CancellationToken cancellationToken = default);
     Task<Result<bool>> DeleteAsync(Guid paymentId, CancellationToken cancellationToken = default);
     
     // Payment management operations
     Task<Result<bool>> ExistsAsync(Guid paymentId, CancellationToken cancellationToken = default);
-    Task<Result<List<OrderPayment>>> GetByStatusAsync(short status, CancellationToken cancellationToken = default);
-    Task<Result<List<OrderPayment>>> GetByPaymentMethodAsync(short paymentMethod, CancellationToken cancellationToken = default);
-    Task<Result<List<OrderPayment>>> GetByDateRangeAsync(DateTime fromDate, DateTime toDate, CancellationToken cancellationToken = default);
+    Task<Result<PagedResult<OrderPayment>>> GetByStatusAsync(short status, PagedRequest request, CancellationToken cancellationToken = default);
+    Task<Result<PagedResult<OrderPayment>>> GetByPaymentMethodAsync(short paymentMethod, PagedRequest request, CancellationToken cancellationToken = default);
+    Task<Result<PagedResult<OrderPayment>>> GetByDateRangeAsync(DateTime fromDate, DateTime toDate, PagedRequest request, CancellationToken cancellationToken = default);
     Task<Result<decimal>> GetTotalPaidByOrderAsync(Guid orderId, CancellationToken cancellationToken = default);
     Task<Result<bool>> UpdatePaymentStatusAsync(Guid paymentId, short status, CancellationToken cancellationToken = default);
-    Task<Result<List<OrderPayment>>> GetFailedPaymentsAsync(CancellationToken cancellationToken = default);
+    Task<Result<PagedResult<OrderPayment>>> GetFailedPaymentsAsync(PagedRequest request, CancellationToken cancellationToken = default);
 }
