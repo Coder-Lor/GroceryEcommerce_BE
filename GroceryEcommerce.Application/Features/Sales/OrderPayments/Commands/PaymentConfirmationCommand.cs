ecb1609eb9d4b8c540d7335b7275f59ff0949c75
tuancter <tuanphuong130204@gmail.com>
2025-11-18 19:42:43 +0700

    update api payment confirmation

---
+++ GroceryEcommerce.Application/Features/Sales/OrderPayments/Commands/PaymentConfirmationCommand.cs
@@ -0,0 +1,9 @@
+﻿using GroceryEcommerce.Application.Common;
+using GroceryEcommerce.Application.Models.Sales;
+using MediatR;
+
+namespace GroceryEcommerce.Application.Features.Sales.OrderPayments.Commands;
+
+public record PaymentConfirmationCommand(
+    PaymentConfirmationRequest Request    
+) : IRequest<SepayResponse>;
---
+++ GroceryEcommerce.Application/Features/Sales/OrderPayments/Handlers/PaymentConfirmationHandler.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using GroceryEcommerce.Application.Common;
+using GroceryEcommerce.Application.Features.Sales.OrderPayments.Commands;
+using GroceryEcommerce.Application.Interfaces.Repositories.Sales;
+using GroceryEcommerce.Application.Models.Sales;
+using GroceryEcommerce.Domain.Entities.Sales;
+using Microsoft.Extensions.Logging;
+using MediatR;
+
+
+namespace GroceryEcommerce.Application.Features.Sales.OrderPayments.Handlers;
+
+public class PaymentConfirmationHandler (
+    IOrderPaymentRepository repository,
+    ILogger<CreateOrderPaymentHandler> logger
+) : IRequestHandler<PaymentConfirmationCommand, SepayResponse>
+{
+    public async Task<SepayResponse> Handle(PaymentConfirmationCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            logger.LogInformation("Processing payment confirmation for transaction ID: {TransactionId}", request.Request.Id);
+            // Map the incoming request to the OrderPayment entity
+            var orderPayment = new OrderPayment
+            {
+                PaymentId = Guid.NewGuid(),
+                //OrderId = request.Request.OrderId,
+                PaymentMethod = 3, // Assuming 0 represents SePay
+                Amount = request.Request.TransferAmount,
+                TransactionId = request.Request.ReferenceCode,
+                GatewayResponse = request.Request.Gateway,
+                Status = 1, // Assuming 1 represents 'Completed'
+                PaidAt = request.Request.TransactionDate,
+                CreatedAt = DateTime.UtcNow
+            };
+            var createResult = await repository.CreateAsync(orderPayment, cancellationToken);
+            if (!createResult.IsSuccess)
+            {
+                logger.LogError("Failed to create order payment from payment confirmation");
+                return new SepayResponse
+                {
+                    success = false,
+                    message = createResult.ErrorMessage ?? "Failed to process payment confirmation."
+                };
+            }
+            logger.LogInformation("Payment confirmation processed successfully for transaction ID: {TransactionId}", request.Request.Id);
+            return new SepayResponse
+            {
+                success = true,
+                message = "Payment confirmation processed successfully."
+            };
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error processing payment confirmation");
+            return new SepayResponse
+            {
+                success = false,
+                message = "An error occurred while processing payment confirmation."
+            };
+        }
+    }
+}
